9c07b294d30b2d5e50db4141a34e1bda1a30da5d
morning invest <138590263+DEMASIA90@users.noreply.github.com>
2026-08-20 06:55:18 +0900

    Add files via upload

--- docs/news-config.js
+++ docs/news-config.js
@@ -1,3 +1,3 @@
 // Google Apps Script web-app URL (ends with /exec).
 // This URL is public by design; it is not a secret/API key.
-window.BADAK_NEWS_PROXY_URL = 'https://script.google.com/macros/s/AKfycby8vSDy7PiaTQ5LnvROgQOFNanr3Clftg00kKLOhExVOboluCRVmTiARt8ognmkFtHb/exec';
+window.BADAK_NEWS_PROXY_URL = 'PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE';
