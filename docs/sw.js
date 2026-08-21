2152c783d7eb82a4815a5587f922ac5e2aea624d
morning invest <achim8160@gmail.com>
2026-08-21 13:33:35 +0900

    abc

--- docs/sw.js
+++ docs/sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'dtc-pwa-v11-7-shell';
+const CACHE_NAME = 'dtc-pwa-v11-10-shell';
 const SHELL_FILES = [
   './',
   './index.html',
-  './styles.css?v=20260821-v11_8',
-  './runtime-config.js?v=20260821-v11_8',
-  './app.js?v=20260821-v11_8',
-  './news-config.js?v=20260821-v11_8',
-  './pwa.js?v=20260821-v11_8',
+  './styles.css?v=20260821-v11_10',
+  './runtime-config.js?v=20260821-v11_10',
+  './app.js?v=20260821-v11_10',
+  './news-config.js?v=20260821-v11_10',
+  './pwa.js?v=20260821-v11_10',
   './manifest.webmanifest',
-  './logo-dtc.svg?v=20260821-v11_8',
+  './logo-dtc.svg?v=20260821-v11_10',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/icon-512-maskable.png'
