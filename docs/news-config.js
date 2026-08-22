183717ade27adcc913852a3d891c64082cacd728
morning invest <achim8160@gmail.com>
2026-08-22 20:36:49 +0900

    rfgrdfg

--- docs/news-config.js
+++
@@ -1,3 +0,0 @@
-// Google Apps Script web-app URL (ends with /exec).
-// This URL is public by design; it is not a secret/API key.
-window.BADAK_NEWS_PROXY_URL = 'PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE';
